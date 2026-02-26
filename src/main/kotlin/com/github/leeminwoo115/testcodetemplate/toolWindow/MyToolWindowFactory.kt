6eb19b050af4bcc44dcb878de65257e3ecedcdcc
GitHub Action <action@github.com>
2026-02-26 12:57:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/leeminwoo115/testcodetemplate/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/leeminwoo115/testcodetemplate/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.leeminwoo115.testcodetemplate.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.leeminwoo115.testcodetemplate.MyBundle
+import com.github.leeminwoo115.testcodetemplate.services.MyProjectService
 import javax.swing.JButton
 
 
